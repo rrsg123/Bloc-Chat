fa1d35001f77f322857d149dfa1646e2419493a6
rrsg123 <rrsg123@gmail.com>
2017-01-07 09:39:27 -0700

    finish list rooms

---
+++ dist/scripts/controllers/RoomCtrl.js
@@ -0,0 +1,10 @@
+
+(function() {
+    function RoomCtrl(Room) {
+        this.rooms = Room.all;
+    }
+    
+    angular
+        .module('blocChat')
+        .controller('RoomCtrl', ['Room', RoomCtrl]);
+})();
---
+++ dist/scripts/factories/Room.js
@@ -0,0 +1,21 @@
+(function() {
+    function Room($scope, $firebaseArray) {
+        var firebaseRef = new Firebase("https://bloc-chat-ce46d.firebaseio.com");
+        
+        $scope.rooms = $firebaseArray(firebaseRef.child('rooms'));
+        
+        $scope.addRoom = function() {
+            $scope.rooms.$add({
+                name: $scope.newRoomName
+            });
+        };
+        
+        return {
+            all: rooms
+        };
+    }
+
+    angular
+        .module('blocChat')
+        .factory('Room', ['$scope', '$firebaseArray', Room]);
+})();
